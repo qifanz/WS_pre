5f8f05c0a5ca790687f4e50f817a919e080a2d4f
qifanz <qifan.zhang@insa-lyon.fr>
2017-11-06 11:25:15 +0100

    etape1&2

--- src/main/java/caca.java
+++ src/main/java/caca.java
@@ -3,12 +3,18 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import javax.net.ssl.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,11 +23,13 @@ import java.util.regex.Pattern;
  * Created by qifan on 2017/11/6.
  */
 public class caca {
-    public final static String LANG_SEARCH="lang_en";
-    public final static String SEARCH_URL="https://www.google.com/search";
+    public final static String LANG_SEARCH = "lang_fr";
+    public final static String SEARCH_URL = "https://www.google.com/search";
 
-    public static void main(String[] args) throws IOException {
-        //Taking search term input from console
+
+
+    public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the search term.");
         String searchTerm = scanner.nextLine();
@@ -29,9 +37,14 @@ public class caca {
         int num = scanner.nextInt();
         scanner.close();
 
-        String searchURL = SEARCH_URL+ "?q="+searchTerm+"&num="+num+"&lr="+LANG_SEARCH;
+        String searchURL = SEARCH_URL + "?q=" + searchTerm + "&num=" + num + "&lr=" + LANG_SEARCH;
         //without proper User-Agent, we will get 403 error
-        Document doc = Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         //below will print HTML data, save it to a file and open in browser to compare
         //System.out.println(doc.html());
@@ -43,8 +56,21 @@ public class caca {
         for (Element result : results) {
             String linkHref = result.attr("href");
             String linkText = result.text();
-            System.out.println("Text::" + linkText + ", URL::" + linkHref.substring(6, linkHref.indexOf("&")));
+            String url = linkHref.substring(7, linkHref.indexOf("&"));
+            if ((url.contains("http://") || url.contains("https://"))) {
+                System.out.println(url);
+                Document caca = null;
+                try {
+                    caca = Jsoup.connect(url).userAgent("Mozilla/5.0").get();
+                    String scaca = caca.body().select("p").text();
+                    System.out.println(scaca);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            //System.out.println("Text::" + linkText + ", URL::" + linkHref);//.substring(7, linkHref.indexOf("&")));
         }
+
     }
 
 }
