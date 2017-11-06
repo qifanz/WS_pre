901d0a1e2a4859d6105acec55aa9f84425e41d06
qifanz <qifan.zhang@insa-lyon.fr>
2017-11-06 09:41:26 +0100

    jsoup added

--- src/main/java/caca.java
+++ src/main/java/caca.java
@@ -1,5 +1,49 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by qifan on 2017/11/6.
  */
 public class caca {
+
+    public static void main(String[] args) throws IOException {
+        //Taking search term input from console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the search term.");
+        String searchTerm = scanner.nextLine();
+        System.out.println("Please enter the number of results. Example: 5 10 20");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        String searchURL = "https://www.google.com/search" + "?q="+searchTerm+"&num="+num;
+        //without proper User-Agent, we will get 403 error
+        Document doc = Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
+
+        //below will print HTML data, save it to a file and open in browser to compare
+        //System.out.println(doc.html());
+
+        //If google search results HTML change the <h3 class="r" to <h3 class="r1"
+        //we need to change below accordingly
+        Elements results = doc.select("h3.r > a");
+
+        for (Element result : results) {
+            String linkHref = result.attr("href");
+            String linkText = result.text();
+            System.out.println("Text::" + linkText + ", URL::" + linkHref.substring(6, linkHref.indexOf("&")));
+        }
+    }
+
 }
+
